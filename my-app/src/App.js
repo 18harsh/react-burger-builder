d1bbafce6fd16e442b99d5c2820135c0a071e218
Harsh Gandhi <18harshgandhi@gmail.com>
2021-04-09 18:08:38 +0530

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Checkout from './containers/Checkout/Checkout'
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import {Route,Switch} from 'react-router-dom'
+import Orders from './containers/Orders/Orders'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // state = {
+  //   show: true
+  // }
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false })
+  //   },5000)
+  // }
+  render() {
+    return (
+      <div>
+        <Layout>
+          {/* {this.state.show ? <BurgerBuilder /> : null} */}
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>
+            <Route path="/orders" component={Orders} />
+            <Route path="/" component={BurgerBuilder} />
+            
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom' 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './store/reducer'
+
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
